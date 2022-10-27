d8725ef0b8a2e7a821dc1f41060cf5e6159b73a6
Rute-r <rute_ke@hotmail.com>
2022-10-27 18:10:00 -0300

    Video-26-Create-Payment-Method-Screen

--- amazona/backend/models/userModel.js
+++ amazona/backend/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Number, 0 : false, required: true },
+    isAdmin: { type: Number, default: 0, required: true },
   },
   {
     timestamps: true,
